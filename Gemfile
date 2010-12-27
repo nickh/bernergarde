9f3d68e1d9cff63b0f0879f81db8cb99deb7809b
Nick Hengeveld <nickh@verticalresponse.com>
2010-12-27 08:33:56 -0800

    Generated app w/RSpec

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+group :test, :development do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bernergarde::Application.config.secret_token = '0420f8f816d67c5a2fdcd82f2d93f2e7974c2aa97f82288aa4ac5d9959105f90778a16ba8311e99797249623beb9f747e5c3ef53ccd7cea76fbc545834df22a5'
