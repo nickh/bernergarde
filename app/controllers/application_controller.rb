1cea866efcc16b5c7cb072d6620cfaf45c80f5c5
Nick Hengeveld <nickh@hengeveld.com>
2011-06-14 12:15:18 -0700

    Provide a default :locale param where possible

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_locale
 
+  def default_url_options(options={})
+    options.merge(:locale => I18n.locale)
+  end
+
   private
     def set_locale
       I18n.locale = params[:locale] || session[:locale]
